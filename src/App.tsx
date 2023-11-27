1c708e4adcdb700f466d8dbe20c93a19e0d4b4b8
Marina Muñoz <marinacerejidoo@gmail.com>
2023-11-27 16:25:40 +0100

    Create and set values for 3 states

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React, { FC } from "react";
-import "./App.css"
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
+    if(event.target.name === "task"){
+        setTask(event.target.value)
+    }else{
+        setDeadline(Number(event.target.value))
+    }
+
+  }
   return (
     <div className="app">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Type your task" />
-          <input type="number" placeholder="Deadline in days" />
+          <input
+            type="text"
+            name="task"
+            placeholder="Type your task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="deadline"
+            placeholder="Deadline in days"
+            onChange={handleChange}
+          />
         </div>
         <button>Add Task</button>
       </div>
